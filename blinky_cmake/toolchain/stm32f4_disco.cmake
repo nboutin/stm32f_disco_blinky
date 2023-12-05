a29a410aeef2d0bfe74e1cf3d4be24d45ccc0467
nboutin <boutwork@gmail.com>
2023-12-05 22:48:25 +0100

    Improve toolchain

--- blinky_cmake/toolchain/stm32f4_disco.cmake
+++ blinky_cmake/toolchain/stm32f4_disco.cmake
@@ -2,10 +2,20 @@ set(cpu cortex-m4)
 set(fpu fpv4-sp-d16)
 set(float-abi hard)
 
-add_compile_options(-mcpu=${cpu} -mfpu=${fpu} -mfloat-abi=${float-abi} -mthumb)
-add_compile_options($<$<COMPILE_LANGUAGE:ASM>:-masm=auto>)
+message(STATUS "CMAKE_C_COMPILER_ID: ${CMAKE_C_COMPILER_ID}")
 
-add_link_options(-mcpu=${cpu} -mfpu=${fpu} -mfloat-abi=${float-abi})
+add_compile_options(
+  -mcpu=${cpu}
+  $<$<COMPILE_LANG_AND_ID:C,GNU>:-mfpu=${fpu}>
+  -mfloat-abi=${float-abi}
+  $<$<COMPILE_LANGUAGE:ASM>:"-masm=auto">
+  -mthumb
+)
+
+add_link_options(
+  $<$<COMPILE_LANG_AND_ID:C,ARMClang>:"--cpu=${cpu}">
+  $<$<COMPILE_LANG_AND_ID:C,GNU>:"-mcpu=${cpu} -mfpu=${fpu} -mfloat-abi=${float-abi}">
+)
 
 # set(CMAKE_ASM_FLAGS "-mcpu=${cpu} -mfpu=${fpu} -mfloat-abi=${float-abi}")
 # set(CMAKE_C_FLAGS "-mcpu=${cpu} -mfpu=${fpu} -mfloat-abi=${float-abi}")
