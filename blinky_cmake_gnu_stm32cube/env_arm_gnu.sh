c7074042ff9c20b68fa652bbafcddc31132783e5
Nicolas Boutin <boutwork@gmail.com>
2023-12-06 22:00:36 +0100

    Add blinky cmake gnu stm32cube working!!

---
+++ blinky_cmake_gnu_stm32cube/env_arm_gnu.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export PATH="/d/program/ninja-win:$PATH"
+# export PATH="/d/program/arm-gnu-toolchain-13.2.Rel1-mingw-w64-i686-arm-none-eabi/bin:$PATH"
+export PATH="/d/program/arm-gnu-tools-for-stm32.11.3.rel1.win32_1.1.100.202309141235/tools/bin:$PATH"
