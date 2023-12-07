bd01d8fe8939c873bddeffead0f300215af8d738
Nicolas Boutin <boutwork@gmail.com>
2023-12-07 22:13:43 +0100

    Improve CMake

--- blinky_cmake/cmake/binary_size.cmake
+++ blinky_cmake/cmake/binary_size.cmake
@@ -2,5 +2,6 @@
 add_custom_command(TARGET ${target_name} POST_BUILD
   COMMENT "Compute binary size"
   WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
-  COMMAND ${CMAKE_SIZE_UTIL} "${target_name}.elf"
+  COMMAND ${CMAKE_SIZE_UTIL} -G -x ${target_name}.elf
+  # COMMAND ${CMAKE_SIZE_UTIL} -A -x ${target_name}.elf
 )
